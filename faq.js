3ba7d1e5bd56d07caba50cb7b56e82c001472f96
Caitlin Hines <chines@healthslate.com>
2017-11-21 11:01:17 -0800

    add Nokia html page

--- faq.js
+++ faq.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	let hash = document.location.hash;
-	
+
 	if(hash){
 		$(hash).find('a').click();
-	}	
+	}
 
-	
+
 	$('html, body').animate({
 		scrollTop: $(hash).offset().top
 	}, 'slow');
 
-	
+
 });
